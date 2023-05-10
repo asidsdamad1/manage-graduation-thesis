aa542768bb2200aa02c8605fadcdfd20cf0c8a37
hieunt <hieunt@elcom.com.vn>
2023-05-10 18:07:40 +0700

    edit, delete event calendar

--- src/main/java/utc/edu/thesis/controller/ReminderController.java
+++ src/main/java/utc/edu/thesis/controller/ReminderController.java
@@ -2,10 +2,7 @@ package utc.edu.thesis.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import utc.edu.thesis.domain.dto.ProjectDto;
 import utc.edu.thesis.domain.dto.ReminderDto;
 import utc.edu.thesis.domain.dto.SearchDto;
@@ -27,5 +24,15 @@ public class ReminderController {
     @PostMapping("/add-reminder")
     public ResponseEntity<ReminderDto> addReminder(@RequestBody ReminderDto dto) {
         return ResponseEntity.ok(reminderService.addReminder(dto));
+    }
+
+    @PostMapping("/edit-reminder")
+    public ResponseEntity<ReminderDto> editReminder(@RequestBody ReminderDto dto) {
+        return ResponseEntity.ok(reminderService.editReminder(dto));
+    }
+
+    @PostMapping("/delete-reminder/{id}")
+    public ResponseEntity<Boolean> deleteReminder(@PathVariable Long id) {
+        return ResponseEntity.ok(reminderService.deleteReminder(id));
     }
 }
--- src/main/java/utc/edu/thesis/service/impl/ReminderServiceImpl.java
+++ src/main/java/utc/edu/thesis/service/impl/ReminderServiceImpl.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import utc.edu.thesis.domain.dto.*;
 import utc.edu.thesis.domain.entity.Reminder;
+import utc.edu.thesis.domain.entity.Student;
+import utc.edu.thesis.exception.request.BadRequestException;
 import utc.edu.thesis.exception.request.NotFoundException;
 import utc.edu.thesis.repository.ReminderRepository;
 import utc.edu.thesis.service.ReminderService;
@@ -64,5 +66,36 @@ public class ReminderServiceImpl implements ReminderService {
             return ReminderDto.of(reminder);
         }
         return null;
+    }
+
+    @Override
+    public ReminderDto editReminder(ReminderDto dto) {
+        if (dto.getId() == null) {
+            throw new BadRequestException("null");
+        }
+
+        Reminder reminder = Reminder.builder()
+                .id(dto.getId())
+                .title(dto.getTitle())
+                .classNames(dto.getClassNames())
+                .start(dto.getStart())
+                .end(dto.getEnd())
+                .teacher(TeacherDto.toEntity(dto.getTeacher()))
+                .session(SessionDto.toEntity(dto.getSession()))
+                .build();
+
+        return ReminderDto.of(reminderRepository.save(reminder));
+    }
+
+    @Override
+    public Boolean deleteReminder(Long id) {
+        if (id != null) {
+            ReminderDto res = reminderRepository.findById(id).map(ReminderDto::of).orElseThrow();
+            if (res != null) {
+                reminderRepository.delete(ReminderDto.toEntity(res));
+                return true;
+            }
+        }
+        return false;
     }
 }
